da65b4d986d7cd540cea181ae1b0be775280ad85
keunwoo <avolic@kaist.ac.kr>
2014-10-07 14:51:45 +0900

    git ignore makefiles and starting LCD Demo

--- FreeRTOS_Kernel/Demo_Blinking_GPIO_8/RaspberryPi-FreeRTOS-build-140523-3/dbuild.config.mk
+++ FreeRTOS_Kernel/Demo_Blinking_GPIO_8/RaspberryPi-FreeRTOS-build-140523-3/dbuild.config.mk
@@ -4,4 +4,5 @@ CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
 CFLAGS += -I $(BASE)FreeRTOS/Source/include/
 CFLAGS += -I $(BASE)Demo/Drivers/
 
-TOOLCHAIN=arm-none-eabi-
+#TOOLCHAIN=arm-none-eabi-
+TOOLCHAIN=/usr/local/gcc-arm-none-eabi-4_8-2014q3/bin/arm-none-eabi-
